8da1c143a8da8f2b3d3c5fdb4aaaf26c1e442b01
devExploitt <smart.romanova15@gmail.com>
2023-08-05 15:33:33 +0400

    init

---
+++ src/Components/Count/Count.jsx
@@ -0,0 +1,19 @@
+import cn from 'classnames';
+import s from './Count.module.scss';
+import className from 'classnames';
+
+export const Count = ({
+    count,
+    handleIncrement,
+    handleDecrement,
+}) => (
+    <div className={cn(s.count, className)}>
+        <button className={s.item} type='button' onClick={handleDecrement}>
+            -
+        </button>
+        <span className={cn(s.item, s.number)} >{count}</span>
+        <button className={s.item} type='button' onClick={handleIncrement}>
+            +
+        </button>
+    </div>
+);
